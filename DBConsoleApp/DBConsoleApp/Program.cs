80c6fa0bd9470eb6ef40fb69b9f2987c04b9b47c
marcoronaa <98579099+marcoronaa@users.noreply.github.com>
2022-02-01 14:11:10 +0100

    prova

---
+++ DBConsoleApp/DBConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Data.SqlClient;
+
+try
+{
+    string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        Console.WriteLine("\nQuery data example:");
+        Console.WriteLine("=========================================\n");
+
+        connection.Open();
+
+        String sql = "SELECT FirstName, LastName FROM Employees";
+
+        using (SqlCommand command = new SqlCommand(sql, connection))
+        {
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                }
+            }
+        }
+    }
+}
+catch (SqlException e)
+{
+    Console.WriteLine(e.ToString());
+}
+Console.WriteLine("\nDone. Press enter.");
+Console.ReadLine();
